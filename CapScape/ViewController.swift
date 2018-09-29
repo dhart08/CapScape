de13cb0c9ac3ef923c139453ba3d64e6e415a5d3
David <david@Davids-Mac.local>
2018-09-29 04:39:06 -0700

    added photo capture animation

--- CapScape/ViewController.swift
+++ CapScape/ViewController.swift
@@ -16,9 +16,10 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate{
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var snapshotButton: UIButton!
     @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var latitudeValueLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
-    @IBOutlet weak var statusLabel: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var longitudeValueLabel: UILabel!
+    @IBOutlet weak var photoPreview: UIImageView!
     
     let videoCapture = VideoCapture()
     
@@ -33,16 +34,6 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate{
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        let worker1 = DispatchQueue(label: "worker1")
-//        let worker2 = DispatchQueue(label: "worker2")
-//
-//        worker1.async {
-//            //code here
-//        }
-//        worker2.async {
-//            //code here
-//        }
-        
         NotificationCenter.default.addObserver(self, selector: #selector(onDidReceiveImage(_:)), name: .didReceiveImage, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(onDidReceiveCoordinates(_:)), name: .didReceiveCoordinates, object: nil)
         
@@ -53,6 +44,9 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate{
         
         locationFinder = LocationFinder()
         locationFinder.requestAuthorization()
+        
+        let photoPreviewTapGesture = UITapGestureRecognizer(target: self, action:#selector(photoViewSegue))
+        photoPreview.addGestureRecognizer(photoPreviewTapGesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -60,6 +54,11 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate{
         
         cameraView.bringSubview(toFront: recordButton)
         cameraView.bringSubview(toFront: snapshotButton)
+        cameraView.bringSubview(toFront: latitudeLabel)
+        cameraView.bringSubview(toFront: latitudeValueLabel)
+        cameraView.bringSubview(toFront: longitudeLabel)
+        cameraView.bringSubview(toFront: longitudeValueLabel)
+        cameraView.bringSubview(toFront: photoPreview)
         
         locationFinder.startFindingLocation()
     }
@@ -72,6 +71,15 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate{
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func photoViewSegue(gesture: UIGestureRecognizer) {
+        if (photoPreview == gesture.view as? UIImageView) {
+            print("photoViewSegue")
+        }
+        else {
+            print("not photoPreview")
+        }
     }
 
     @IBAction func recordButtonClick(_ sender: UIButton) {
@@ -88,20 +96,37 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate{
     }
     
     @IBAction func snapshotButtonClick(_ sender: UIButton) {
-        photoCapture.takePhoto()
+        let photoThread = DispatchQueue(label: "photoThread")
+        photoThread.async {
+            self.photoCapture.takePhoto()
+        }
     }
     
     @objc func onDidReceiveImage(_ notification: Notification) {
         currentPhoto = photoCapture.getPhoto()
-        imageView.image = currentPhoto
+        photoPreview.image = currentPhoto
+        
+        let minSize: CGSize = photoPreview.frame.size
+        let minX: CGFloat = photoPreview.frame.origin.x
+        let minY: CGFloat = photoPreview.frame.origin.y
+        
+        photoPreview.alpha = 0.0
+        photoPreview.frame = CGRect(x: 0, y: 0, width: cameraView.frame.width, height: cameraView.frame.height)
+        
+        UIView.animate(withDuration: 0.2){
+            self.photoPreview.alpha = 1
+            self.photoPreview.frame.size = minSize
+            self.photoPreview.frame.origin.x = minX
+            self.photoPreview.frame.origin.y = minY
+        }
     }
     
     @objc func onDidReceiveCoordinates(_ notification: Notification) {
         let (latDeg, latMin, latSec) = locationFinder.decimalToDegrees(coordinate: locationFinder.latitude)
         let (lonDeg, lonMin, lonSec) = locationFinder.decimalToDegrees(coordinate: locationFinder.longitude)
         
-        latitudeLabel.text = "\(latDeg) \(latMin)' \(latSec)''"
-        longitudeLabel.text = "\(lonDeg) \(lonMin)' \(lonSec)''"
+        latitudeValueLabel.text = "\(latDeg) \(latMin)' \(latSec)''"
+        longitudeValueLabel.text = "\(lonDeg) \(lonMin)' \(lonSec)''"
     }
     
 }
